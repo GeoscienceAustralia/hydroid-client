f9f0048df4254c2a2244bc7ca6e35013713bd716
Darren Reid <darren.j.reid@gmail.com>
2016-01-18 17:35:04 +1100

    Update server.js to use optional proxy
    Quick impl of search working with temporary stanbol instance.

--- app/components/search/search.js
+++ app/components/search/search.js
@@ -2,33 +2,47 @@
 (function () {
     "use strict";
 
-    var module = angular.module('search', [
+    var module = angular.module('search', []);
 
-    ]);
-
-    module.directive('hydroidSearch', ['$http',function($http) {
+    module.directive('hydroidSearch', ['$http', '$timeout', function ($http, $timeout) {
         return {
             restrict: 'E',
             scope: {
-                results:'=',
-                queryUrl:'@',
+                results: '=',
+                queryUrl: '@',
                 contentHubName: '@'
             },
             templateUrl: 'components/search/search.html',
-            controller: ['$scope', function($scope) {
+            controller: ['$scope', function ($scope) {
+                $scope.factAliases = {
+                    'organizations_t':'Organisations',
+                    'people_t': 'People',
+                    'places_t': 'Places'
+                };
                 $scope.search = function (query) {
                     $http.get($scope.queryUrl + '/contenthub/' +
-                        $scope.contentHubName +
-                        '/search/featured?queryTerm=' + query)
+                            $scope.contentHubName +
+                            '/search/featured?queryTerm=' + query)
                         .then(function (response) {
-                            console.log(response.data);
-                        },
-                        function (response) {
-                            console.log('error in api request');
-                        });
+                                console.log(response.data);
+                                $timeout(function () {
+                                    $scope.results = {data: response.data.documents, categories: response.data.facets};
+                                    for(var i = 0; i < $scope.results.categories.length - 1; i++) {
+                                        var cat = $scope.results.categories[i];
+                                        cat.facet.alias = getFacetAlias(cat.facet.name);
+                                    }
+                                });
+                            },
+                            function (response) {
+                                console.log('error in api request');
+                            });
                 };
+
+                function getFacetAlias(facetName) {
+                    return $scope.factAliases[facetName];
+                }
             }],
-            link: function(scope,element, attrs) {
+            link: function (scope, element, attrs) {
 
             }
         };
--- server.js
+++ server.js
@@ -2,23 +2,49 @@ var express = require('express');
 var app = express();
 
 var http = require('http');
+var fs = require('fs');
+var config = null;
+if(fs.existsSync('local-config.json')) {
+    config = require('local-config.json');
+} else {
+    config = {
+        useProxy: false,
+        stanbolPath: 'http://hydroid-dev-web-lb-1763223935.ap-southeast-2.elb.amazonaws.com/stanbol'
+        //stanbolPath: 'http://localhost:8080/'
+    }
+}
 
 app.use('/', express.static(__dirname + '/app'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use('/api', function (request, response) {
 
-    var stanbolUrl = request.originalUrl.replace('/api/','http://localhost:8080/');
-    var proxy = http.request(stanbolUrl, function (res) {
-        res.pipe(response, {
-            end: true
-        })
-    }).on('error', function (err) {
+    var stanbolUrl = request.originalUrl.replace('/api/',config.stanbolPath);
+    var proxy;
+    if(config.useProxy) {
+        var options = {
+            host: config.proxy.host,
+            port: config.proxy.port,
+            path: stanbolUrl
+        };
+        proxy = http.request(options, function (res) {
+            res.pipe(response, {
+                end: true
+            })
+        });
+    } else {
+        proxy = http.request(stanbolUrl, function (res) {
+            res.pipe(response, {
+                end: true
+            })
+        });
+    }
+
+    proxy.on('error', function (err) {
         return(err);
     });
 
     // Use default auth for using local instance of stanbol
-    proxy.setHeader('Authorization', 'Basic YWRtaW46YWRtaW4=');
     proxy.setHeader('Content-Type', 'application/json');
     proxy.setHeader('Accept', 'application/json');
 
