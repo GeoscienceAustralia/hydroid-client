ecd2d2460008dabd400af4738267cdbd2b01e52c
scelton <elton.carneiro@ga.gov.au>
2016-02-05 16:19:36 +1100

    Menu content updated and simple facet filter added

--- app/components/search/search-related.js
+++ app/components/search/search-related.js
@@ -9,14 +9,17 @@
         return {
             restrict: 'E',
             scope: {
+                results: '=',
                 menuItems:'=',
-                queryUrl: '@'
+                queryUrl: '@',
+                solrCollection: '@',
+                menuUrl: '@'
             },
             templateUrl: 'components/search/search-related.html',
             controller: ['$scope', function($scope) {
 
                 $scope.buildMenu = function () {
-                    $http.get($scope.queryUrl)
+                    $http.get($scope.menuUrl)
                         .then(function (response) {
                             console.log(response.data);
                             $timeout(function () {
@@ -29,6 +32,43 @@
                 };
 
                 $scope.buildMenu();
+
+                $scope.filterByFacet = function(facet) {
+                    $http.get($scope.queryUrl + '/' + $scope.solrCollection +
+                        '/select?q=*&facet=true&facet.field=label&fq=' + facet + '&wt=json')
+                        .then(function (response) {
+                            console.log(response.data);
+                            $timeout(function () {
+                                $scope.results = {docs: response.data.response.docs, facets: response.data.facet_counts};
+                                var facetStats = getFacetStats($scope.results.facets);
+                                updateCounters(facetStats, $scope.menuItems);
+                            });
+                        },
+                        function (response) {
+                            console.log('error in api request');
+                        });
+                };
+
+                function getFacetStats(facets) {
+                    var facetStats = [];
+                    for (var i=0; i < facets.facet_fields.label.length; i=i+2) {
+                        facetStats.push({ facet: facets.facet_fields.label[i], count: facets.facet_fields.label[i+1]});
+                    }
+                    return facetStats;
+                }
+
+                function updateCounters(facetStats, menuItems) {
+                    for (var i=0; i < facetStats.length; i++) {
+                        for (var j=0; j < menuItems.length; j++) {
+                            if (menuItems[j].nodeLabel === facetStats[i].facet) {
+                                menuItems[j].count = facetStats[i].count;
+                            }
+                            if (menuItems[j].children) {
+                                updateCounters(facetStats, menuItems[j].children);
+                            }
+                        }
+                    }
+                }
 
             }]
         };
--- app/components/search/search.js
+++ app/components/search/search.js
@@ -15,11 +15,7 @@
             },
             templateUrl: 'components/search/search.html',
             controller: ['$scope', function ($scope) {
-                $scope.factAliases = {
-                    'organizations_t':'Organisations',
-                    'people_t': 'People',
-                    'places_t': 'Places'
-                };
+
                 $scope.search = function (query) {
                     $http.get($scope.queryUrl + '/' + $scope.solrCollection +
                             '/select?q=' + query + '*&facet=true&facet.field=label&wt=json')
@@ -27,13 +23,7 @@
                                 console.log(response.data);
                                 $timeout(function () {
                                     $scope.results = {docs: response.data.response.docs, facets: response.data.facet_counts};
-
-                                    var facets = $scope.results.facets;
-                                    var facetStats = [];
-                                    for (var i=0; i < facets.facet_fields.label.length; i=i+2) {
-                                        facetStats.push({ facet: facets.facet_fields.label[i], count: facets.facet_fields.label[i+1]});
-                                    }
-
+                                    var facetStats = getFacetStats($scope.results.facets);
                                     updateCounters(facetStats, $scope.menuItems);
                                 });
                             },
@@ -42,8 +32,12 @@
                             });
                 };
 
-                function getFacetAlias(facetName) {
-                    return $scope.factAliases[facetName];
+                function getFacetStats(facets) {
+                    var facetStats = [];
+                    for (var i=0; i < facets.facet_fields.label.length; i=i+2) {
+                        facetStats.push({ facet: facets.facet_fields.label[i], count: facets.facet_fields.label[i+1]});
+                    }
+                    return facetStats;
                 }
 
                 function updateCounters(facetStats, menuItems) {
