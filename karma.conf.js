192451ef6cb59b506b360dabce13cfddec85cea7
Darren Reid <darren.j.reid@gmail.com>
2016-02-09 15:41:04 +1100

    Polyfill phantomjs 1.9.x for angular tests

--- karma.conf.js
+++ karma.conf.js
@@ -13,6 +13,7 @@ module.exports = function (config) {
             'node_modules/angular/angular.js',
             'node_modules/angular-mocks/angular-mocks.js',
             'node_modules/angular-route/angular-route.js',
+            'node_modules/phantomjs-function-bind-polyfill/index.js',
             'app/components/**/*.js',
             'app/components/**/*.html'
         ],
