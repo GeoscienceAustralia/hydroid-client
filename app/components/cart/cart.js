7b7f93e45757195427afde24431755a8c28c42c2
mikilovski <mitko.ilovski@gmail.com>
2016-03-01 11:06:55 +1100

    Added remove from list button and functionality

--- app/components/cart/cart.js
+++ app/components/cart/cart.js
@@ -29,12 +29,25 @@
                         bytes: 250445
                     }
                 ]
+
                 $scope.getTotalBytes = function() {
                     var totalBytes = 0;
                     for(var i = 0; i < $scope.cartItems.length; i++) {
                         totalBytes += $scope.cartItems[i].bytes;
                     }
                     return totalBytes;
+                }
+                $scope.removeItemFromList = function(item) {
+                    var index = $scope.cartItems.indexOf(item);
+                    $scope.cartItems.splice(index, 1);
+                }
+                $scope.downloadCartItems = function() {
+                    //Zip array of items here and download
+
+                    //Dummy download test
+                    for(var item in $scope.cartItems) {
+                        console.log("Downloading item .. " + $scope.cartItems[item].name);
+                    }
                 }
             }]
         }
