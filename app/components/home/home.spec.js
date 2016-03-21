888830a28e9537ad43146ea9f4c286cca9ad537f
Darren Reid <darren.reid@ga.gov.au>
2016-03-21 16:20:59 +1100

    Fix home spec test

--- app/components/home/home.spec.js
+++ app/components/home/home.spec.js
@@ -2,9 +2,10 @@
 
 describe('Hydroid home components tests', function () {
     var $compile,
+        $httpBackend,
         $rootScope;
 
-    angular.module('mockApp', [
+    angular.module('mockHomeApp', [
         'ngMock',
         'home'
     ]);
@@ -13,13 +14,14 @@ describe('Hydroid home components tests', function () {
     beforeEach(module('components/home/home.html'));
 
     // Load the myApp module, which contains the directive
-    beforeEach(module('mockApp'));
+    beforeEach(module('mockHomeApp'));
 
     // Store references to $rootScope and $compile
     // so they are available to all tests in this describe block
-    beforeEach(inject(function(_$compile_, _$rootScope_){
+    beforeEach(inject(function(_$compile_, _$rootScope_,_$httpBackend_){
         // The injector unwraps the underscores (_) from around the parameter names when matching
         $compile = _$compile_;
+        $httpBackend = _$httpBackend_;
         $rootScope = _$rootScope_;
     }));
 
