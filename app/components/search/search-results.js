d49ef7b4c77ba21f53860e310ead2d4a2b1d2436
Darren Reid <darren.reid@ga.gov.au>
2016-05-24 13:57:01 +1000

    Reinit next page on collection change.

--- app/components/search/search-results.js
+++ app/components/search/search-results.js
@@ -151,6 +151,7 @@
                         if ($scope.docType === 'IMAGE') {
                             $scope.imageRows = SearchServices.getResultImageRows($scope.documents, totalsRows);
                         }
+                        currentPage = (newVal.length / totalsRows);
                         $scope.hasNextPage = $scope.numFound > (totalsRows * (currentPage + 1));
                     }
                 });
